9a0ea1bcee534d50c234a97ada8201e8f067aedf
Stefersongc <steferson2030@hotmail.com>
2025-11-30 17:04:13 +0000

    Criando Hero e Importação de Fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
+// Task para compilar SASS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-};
+// Task para comprimir imagens (rodar quando quiser)
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+// Task de observação apenas do SASS (sem imagens)
+function watchFiles() {
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./index.html');
+}
+
+// Exportações
+exports.styles = styles;          // roda: npm run styles ou gulp styles
+exports.images = images;          // roda: npm run images ou gulp images
+exports.watch = watchFiles;       // roda: npm run watch ou gulp watch
+
+// Build básico (só SASS)
+exports.build = gulp.parallel(styles);
+
+// Build completo (SASS + imagens, só quando quiser)
+exports.default = gulp.parallel(styles, images);
